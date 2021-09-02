fb89c56ece495167dd9f2af51e2154f50b1f26c3
Alberto Gila Navarro <60033875+AlbertoGilaNavarro@users.noreply.github.com>
2021-09-02 20:16:48 +0200

    Update train&validate_MLPModel.py

--- train&validate_MLPModel.py
+++ train&validate_MLPModel.py
@@ -7,7 +7,7 @@ from tensorflow.keras.optimizers import Adam
 import myNeuralNetworks
 
 #%% Load the data
-#You must create your compressed 2D-array starting with the 3D-arrays provided in the dataset. Check the script arraysCompression.py
+#You must create your compressed 2D-array starting with the 3D-arrays provided in the dataset. Check the script MSIcompression.py
 trainX=pd.read_table("channelMeansTrain.txt", index_col=0, sep="\t")
 valX=pd.read_table("channelMeansTrain.txt", index_col=0, sep="\t")
 testX=pd.read_table("channelMeansTest.txt", index_col=0, sep="\t")
